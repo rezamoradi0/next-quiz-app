ded1fc9e921bfdc903ec254a8bcffd818cf5ba13
rezamoradi0 <REZAMORADI0933@GMAIL.COM>
2024-03-05 13:00:55 +0330

    Popup Animated

--- components/modules/popup/PopupFull.jsx
+++ components/modules/popup/PopupFull.jsx
@@ -1,13 +1,13 @@
 import { createPortal } from "react-dom";
-
+import {motion} from "framer-motion"
 function PopupFull({ children }) {
 
   return createPortal(
     <>
         
-   <div className="fixed left-0 top-0  bottom-0 z-50 h-screen  w-screen overflow-x-hidden bg-zinc-800 p-4 ">
+   <motion.div animate={{width:["0vw","100vw"] ,maxHeight:["0vh","100vh"] ,left:["50%","0%"],top:["50%","0%"],transition:{duration:0.2}}}  className="fixed w-0 max-h-0 bottom-0 z-50  overflow-x-hidden bg-zinc-800 p-4">
       {children}
-    </div> 
+    </motion.div> 
 
     </>,
     document.body,
